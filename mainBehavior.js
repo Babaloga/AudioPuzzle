dc3282a82dbf2c9ae4d021eed225b6294c8115d3
Babaloga <31783107+Babaloga@users.noreply.github.com>
2026-05-05 16:39:44 -0400

    New Game Plus

---
+++ mainBehavior.js
@@ -0,0 +1,296 @@
+var bookletPage = 0;
+var functionsPhase = 0;
+
+const page0Address = "";
+const page1Address = "";
+const page2Address = "";
+const page3Address = "";
+
+window.addEventListener("load", (event) => {
+  displayQuestions(); 
+});
+
+function displayBookletImage()
+{
+    var imageAddress = page0Address;
+    if(bookletPage == 1){
+        imageAddress = page1Address;
+    }
+    else if (bookletPage == 2){
+        imageAddress = page2Address;
+    }
+    else if (bookletPage == 3){
+        imageAddress = page3Address;
+    }
+
+    document.getElementById("booklet").src = imageAddress;
+}
+
+function incrementBookletPage(){
+    if(bookletPage < 3) bookletPage++;
+    else bookletPage = 0;
+}
+
+function displayQuestions()
+{
+    var questionContent = questions0Address;
+    if(functionsPhase == 1){
+        questionContent = questions1Address;
+    }
+    else if (functionsPhase == 2){
+        questionContent = questions2Address;
+    }
+    else if (functionsPhase == 3){
+        questionContent = questions3Address;
+    }
+    else if (functionsPhase == 4){
+        questionContent = questions4Address;
+    }
+    else if (functionsPhase == 5){
+        questionContent = '<h1>Congratulations, Detective</h1>';
+    }
+
+    document.getElementById("questions").innerHTML = questionContent;
+}
+
+function checkAnswers()
+{
+    if(functionsPhase == 0)
+    {
+        if(!document.querySelector('input[name="Where is this microphone located?"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+
+        if(document.querySelector('input[name="Where is this microphone located?"]:checked').value == "G")
+        {
+            functionsPhase = 1;
+            displayQuestions(); 
+        }
+    }
+    else if(functionsPhase == 1){
+        var correct = 0;
+
+        if(!document.querySelector('input[name="Who is carrying this microphone?"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+        if(!document.querySelector('input[name="Who died?"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+        if(!document.querySelector('input[name="Where did the death occur?"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+
+        if(document.querySelector('input[name="Who is carrying this microphone?"]:checked').value == "B")
+        {
+            correct++;
+        }
+        if(document.querySelector('input[name="Who died?"]:checked').value == "B")
+        {
+            correct++;
+        }
+        if(document.querySelector('input[name="Where did the death occur?"]:checked').value == "A")
+        {
+            correct++;
+        }
+
+        if(correct == 3)
+        {
+            document.getElementById("feedback").innerHTML = "";
+            functionsPhase = 2;
+            displayQuestions();
+        }
+        else
+        {
+            document.getElementById("feedback").innerHTML = correct + " Correct";
+        }
+    }
+    else if (functionsPhase == 2){
+        var correct = 0;
+
+        if(!document.querySelector('input[name="Where is Mic 2 located?"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+        
+        if(document.querySelectorAll('input[name="Who is in the room at the beginning of the scene?"]:checked').length < 1)
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+
+        if(document.querySelectorAll('input[name="Who is in the room at the end of the scene?"]:checked').length < 1)
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+
+        if(document.querySelector('input[name="Where is Mic 2 located?"]:checked').value == "E")
+        {
+            correct++;
+        }
+
+        const highlightedItems = document.querySelectorAll('input[name="Who is in the room at the beginning of the scene?"]:checked');
+        const correctAnswers = ["A", "B", "F"];
+        
+        var correctCheckboxes = 0;
+        
+        highlightedItems.forEach(function(selection)
+        {
+            for (let x of correctAnswers) {
+                if (x == selection.value) {
+                    correctCheckboxes++;
+                    return;
+                }
+            }
+            correctCheckboxes = -100;
+        });
+
+        if(correctCheckboxes == 3)
+        {
+            correct++;
+        }
+
+        const highlightedItems2 = document.querySelectorAll('input[name="Who is in the room at the end of the scene?"]:checked');
+        const correctAnswers2 = ["A", "C"];
+
+        correctCheckboxes = 0;
+
+        highlightedItems2.forEach(function(selection) 
+        {
+            for (let x of correctAnswers2) {
+                if (x == selection.value) {
+                    correctCheckboxes++;
+                    return;
+                }
+            }
+            correctCheckboxes = -100;
+        });
+
+        if(correctCheckboxes == 2)
+        {
+            correct++;
+        }
+        
+        if(correct == 3)
+        {
+            document.getElementById("feedback").innerHTML = "";
+            functionsPhase = 3;
+            displayQuestions();
+        }
+        else
+        {
+            document.getElementById("feedback").innerHTML = correct + " Correct";
+        }
+    }
+    else if (functionsPhase == 3){
+        var correct = 0;
+
+        if(!document.querySelector('input[name="Where is Mic 3 located?"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+
+        if(document.querySelectorAll('input[name="What was the cause of death?"]:checked').length < 1)
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+
+        if(!document.querySelector('input[name="How many people have been on the catwalk?"]').value)
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+
+        if(document.querySelector('input[name="Where is Mic 3 located?"]:checked').value == "C")
+        {
+            correct++;
+        }
+        
+        if(document.querySelector('input[name="What was the cause of death?"]:checked').value == "B")
+        {
+            correct++;
+        }
+
+        if(document.querySelector('input[name="How many people have been on the catwalk?"]').value == "3")
+        {
+            correct++;
+        }
+
+        if(correct == 3)
+        {
+            document.getElementById("feedback").innerHTML = "";
+            functionsPhase = 4;
+            displayQuestions();
+        }
+        else
+        {
+            document.getElementById("feedback").innerHTML = correct + " Correct";
+        }
+    }
+    else if (functionsPhase == 4){
+        var correct = 0;
+
+        if(!document.querySelector('input[name="lighting assistant"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+        if(!document.querySelector('input[name="stage manager"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+        if(!document.querySelector('input[name="understudy"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+        if(!document.querySelector('input[name="sound designer"]:checked'))
+        {
+            document.getElementById("feedback").innerHTML = "You must give an answer to each question.";
+            return;
+        }
+
+        if(document.querySelector('input[name="lighting assistant"]:checked').value == "E")
+        {
+            correct++;
+        }
+        
+        if(document.querySelector('input[name="stage manager"]:checked').value == "C")
+        {
+            correct++;
+        }
+        
+        if(document.querySelector('input[name="understudy"]:checked').value == "D")
+        {
+            correct++;
+        }
+        
+        if(document.querySelector('input[name="sound designer"]:checked').value == "G")
+        {
+            correct++;
+        }
+
+        if(correct == 4)
+        {
+            document.getElementById("feedback").innerHTML = "";
+            functionsPhase = 5;
+            displayQuestions();
+        }
+        else
+        {
+            document.getElementById("feedback").innerHTML = correct + " Correct";
+        }
+    }
+}
---
+++ questionContent.js
@@ -0,0 +1,217 @@
+const questions0Address = '<form> \
+    <h3>Where is this microphone located?</h3> \
+    <input type="radio" id="a" name="Where is this microphone located?" value="A"> \
+    <label for="a">Catwalks</label><br> \
+    <input type="radio" id="b" name="Where is this microphone located?" value="B"> \
+    <label for="b">Costumes</label><br> \
+    <input type="radio" id="c" name="Where is this microphone located?" value="C"> \
+    <label for="c">Backstage</label><br> \
+    <input type="radio" id="d" name="Where is this microphone located?" value="D"> \
+    <label for="d">Downstairs hallway</label><br> \
+    <input type="radio" id="e" name="Where is this microphone located?" value="E"> \
+    <label for="e">Dressing Room</label><br>\
+    <input type="radio" id="f" name="Where is this microphone located?" value="F">\
+    <label for="f">Props</label><br>\
+    <input type="radio" id="g" name="Where is this microphone located?" value="G">\
+    <label for="g">On Someone\'s Person</label><br>\
+    <br/>\
+</form>';
+
+const questions1Address = '<form>\
+<h3>Who is carrying this microphone?</h3>\
+<input type="radio" id="a" name="Who is carrying this microphone?" value="A">\
+<label for="a">Lighting assistant</label><br>\
+<input type="radio" id="b" name="Who is carrying this microphone?" value="B">\
+<label for="b">Producer</label><br>\
+<input type="radio" id="c" name="Who is carrying this microphone?" value="C">\
+<label for="c">Stage manager</label><br>\
+<input type="radio" id="d" name="Who is carrying this microphone?" value="D">\
+<label for="d">Sound director</label><br>\
+<input type="radio" id="e" name="Who is carrying this microphone?" value="E">\
+<label for="e">Understudy</label><br>\
+<br/>\
+\
+<h3>Who died?</h3>\
+<input type="radio" id="a" name="Who died?" value="A">\
+<label for="a">Lighting assistant</label><br>\
+<input type="radio" id="b" name="Who died?" value="B">\
+<label for="b">Producer</label><br>\
+<input type="radio" id="c" name="Who died?" value="C">\
+<label for="c">Stage manager</label><br>\
+<input type="radio" id="d" name="Who died?" value="D">\
+<label for="d">Sound director</label><br>\
+<input type="radio" id="e" name="Who died?" value="E">\
+<label for="e">Understudy</label><br>\
+<br/>\
+\
+<h3>Where did the death occur?</h3>\
+<input type="radio" id="a" name="Where did the death occur?" value="A">\
+<label for="a">Catwalks</label><br>\
+<input type="radio" id="b" name="Where did the death occur?" value="B">\
+<label for="b">Costumes</label><br>\
+<input type="radio" id="c" name="Where did the death occur?" value="C">\
+<label for="c">Backstage</label><br>\
+<input type="radio" id="d" name="Where did the death occur?" value="D">\
+<label for="d">Downstairs hallway</label><br>\
+<input type="radio" id="e" name="Where did the death occur?" value="E">\
+<label for="e">Dressing Room</label><br>\
+<input type="radio" id="f" name="Where did the death occur?" value="F">\
+<label for="f">Props</label><br>\
+<br/> \
+</form>';
+
+const questions2Address = '<form>\
+    <h3>Where is Mic 2 located?</h3>\
+    <input type="radio" id="a" name="Where is Mic 2 located?" value="A">\
+    <label for="a">Catwalks</label><br>\
+    <input type="radio" id="b" name="Where is Mic 2 located?" value="B">\
+    <label for="b">Costumes</label><br>\
+    <input type="radio" id="c" name="Where is Mic 2 located?" value="C">\
+    <label for="c">Backstage</label><br>\
+    <input type="radio" id="d" name="Where is Mic 2 located?" value="D">\
+    <label for="d">Downstairs hallway</label><br>\
+    <input type="radio" id="e" name="Where is Mic 2 located?" value="E">\
+    <label for="e">Dressing Room</label><br>\
+    <input type="radio" id="f" name="Where is Mic 2 located?" value="F">\
+    <label for="f">Props</label><br>\
+    <input type="radio" id="g" name="Where is Mic 2 located?" value="G">\
+    <label for="g">On Someone\'s Person</label><br>\
+    <br/>\
+\
+    <h3>Who is in the room at the beginning of the scene?</h3>\
+    <input type="checkbox" id="a" name="Who is in the room at the beginning of the scene?" value="A">\
+    <label for="a">Two Chatty Actors</label><br>\
+    <input type="checkbox" id="b" name="Who is in the room at the beginning of the scene?" value="B">\
+    <label for="b">Producer</label><br>\
+    <input type="checkbox" id="c" name="Who is in the room at the beginning of the scene?" value="C">\
+    <label for="c">Lighting Assistant</label><br>\
+    <input type="checkbox" id="d" name="Who is in the room at the beginning of the scene?" value="D">\
+    <label for="d">Sound Engineer</label><br>\
+    <input type="checkbox" id="e" name="Who is in the room at the beginning of the scene?" value="E">\
+    <label for="e">Stage Manager</label><br>\
+    <input type="checkbox" id="f" name="Who is in the room at the beginning of the scene?" value="F">\
+    <label for="f">Understudy</label><br>\
+\
+    <h3>Who is in the room at the end of the scene?</h3>\
+    <input type="checkbox" id="a" name="Who is in the room at the end of the scene?" value="A">\
+    <label for="a">Two Chatty Actors</label><br>\
+    <input type="checkbox" id="b" name="Who is in the room at the end of the scene?" value="B">\
+    <label for="b">Producer</label><br>\
+    <input type="checkbox" id="c" name="Who is in the room at the end of the scene?" value="C">\
+    <label for="c">Lighting Assistant</label><br>\
+    <input type="checkbox" id="d" name="Who is in the room at the end of the scene?" value="D">\
+    <label for="d">Sound Engineer</label><br>\
+    <input type="checkbox" id="e" name="Who is in the room at the end of the scene?" value="E">\
+    <label for="e">Stage Manager</label><br>\
+    <input type="checkbox" id="f" name="Who is in the room at the end of the scene?" value="F">\
+    <label for="f">Understudy</label><br>\
+    <br/>\
+</form>\
+';
+
+const questions3Address = '<form>\
+    <h3>Where is Mic 3 located?</h3>\
+    <input type="radio" id="a" name="Where is Mic 3 located?" value="A">\
+    <label for="a">Catwalks</label><br>\
+    <input type="radio" id="b" name="Where is Mic 3 located?" value="B">\
+    <label for="b">Costumes</label><br>\
+    <input type="radio" id="c" name="Where is Mic 3 located?" value="C">\
+    <label for="c">Backstage</label><br>\
+    <input type="radio" id="d" name="Where is Mic 3 located?" value="D">\
+    <label for="d">Downstairs hallway</label><br>\
+    <input type="radio" id="e" name="Where is Mic 3 located?" value="E">\
+    <label for="e">Dressing Room</label><br>\
+    <input type="radio" id="f" name="Where is Mic 3 located?" value="F">\
+    <label for="f">Props</label><br>\
+    <input type="radio" id="g" name="Where is Mic 3 located?" value="G">\
+    <label for="g">On Someone\'s Person</label><br>\
+    <br/>\
+\
+    <h3>How was the victim killed?</h3>\
+    <input type="radio" id="a" name="What was the cause of death?" value="A">\
+    <label for="a">Electrocuted</label><br>\
+    <input type="radio" id="b" name="What was the cause of death?" value="B">\
+    <label for="b">Hit with blunt object</label><br>\
+    <input type="radio" id="c" name="What was the cause of death?" value="C">\
+    <label for="c">Pushed over railing</label><br>\
+    <input type="radio" id="d" name="What was the cause of death?" value="D">\
+    <label for="d">Shot</label><br>\
+    <input type="radio" id="e" name="What was the cause of death?" value="E">\
+    <label for="e">Strangled</label><br>\
+    <input type="radio" id="f" name="What was the cause of death?" value="F">\
+    <label for="f">Tripped over cables</label><br>\
+    <br/>\
+\
+    <h3>How many people have been on the catwalk?</h3>\
+    <input type="number" id="a" name="How many people have been on the catwalk?" value="A">\
+</form>\
+';
+const questions4Address = '<form>\
+    <h3>At the time of death, where was the lighting assistant?</h3>\
+    <input type="radio" id="a" name="lighting assistant" value="A">\
+    <label for="a">Catwalks</label><br>\
+    <input type="radio" id="b" name="lighting assistant" value="B">\
+    <label for="b">Costumes</label><br>\
+    <input type="radio" id="c" name="lighting assistant" value="C">\
+    <label for="c">Backstage</label><br>\
+    <input type="radio" id="d" name="lighting assistant" value="D">\
+    <label for="d">Downstairs hallway</label><br>\
+    <input type="radio" id="e" name="lighting assistant" value="E">\
+    <label for="e">Dressing Room</label><br>\
+    <input type="radio" id="f" name="lighting assistant" value="F">\
+    <label for="f">Props</label><br>\
+    <input type="radio" id="g" name="lighting assistant" value="G">\
+    <label for="g">COMMITTING A MURDER</label><br>\
+    <br/>\
+\
+    <h3>At the time of death, where was the stage manager?</h3>\
+    <input type="radio" id="a" name="stage manager" value="A">\
+    <label for="a">Catwalks</label><br>\
+    <input type="radio" id="b" name="stage manager" value="B">\
+    <label for="b">Costumes</label><br>\
+    <input type="radio" id="c" name="stage manager" value="C">\
+    <label for="c">Backstage</label><br>\
+    <input type="radio" id="d" name="stage manager" value="D">\
+    <label for="d">Downstairs hallway</label><br>\
+    <input type="radio" id="e" name="stage manager" value="E">\
+    <label for="e">Dressing Room</label><br>\
+    <input type="radio" id="f" name="stage manager" value="F">\
+    <label for="f">Props</label><br>\
+    <input type="radio" id="g" name="stage manager" value="G">\
+    <label for="g">COMMITTING A MURDER</label><br>\
+    <br/>\
+\
+    <h3>At the time of death, where was the understudy?</h3>\
+    <input type="radio" id="a" name="understudy" value="A">\
+    <label for="a">Catwalks</label><br>\
+    <input type="radio" id="b" name="understudy" value="B">\
+    <label for="b">Costumes</label><br>\
+    <input type="radio" id="c" name="understudy" value="C">\
+    <label for="c">Backstage</label><br>\
+    <input type="radio" id="d" name="understudy" value="D">\
+    <label for="d">Downstairs hallway</label><br>\
+    <input type="radio" id="e" name="understudy" value="E">\
+    <label for="e">Dressing Room</label><br>\
+    <input type="radio" id="f" name="understudy" value="F">\
+    <label for="f">Props</label><br>\
+    <input type="radio" id="g" name="understudy" value="G">\
+    <label for="g">COMMITTING A MURDER</label><br>\
+    <br/>\
+\
+    <h3>At the time of death, where was the sound designer?</h3>\
+    <input type="radio" id="a" name="sound designer" value="A">\
+    <label for="a">Catwalks</label><br>\
+    <input type="radio" id="b" name="sound designer" value="B">\
+    <label for="b">Costumes</label><br>\
+    <input type="radio" id="c" name="sound designer" value="C">\
+    <label for="c">Backstage</label><br>\
+    <input type="radio" id="d" name="sound designer" value="D">\
+    <label for="d">Downstairs hallway</label><br>\
+    <input type="radio" id="e" name="sound designer" value="E">\
+    <label for="e">Dressing Room</label><br>\
+    <input type="radio" id="f" name="sound designer" value="F">\
+    <label for="f">Props</label><br>\
+    <input type="radio" id="g" name="sound designer" value="G">\
+    <label for="g">COMMITTING A MURDER</label><br>\
+    <br/>\
+</form>';
